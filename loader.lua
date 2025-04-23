7c9f280a694957252d8b6192f2a5eb534c39463f
Rin or Purse <82947391+nomed-rin@users.noreply.github.com>
2025-04-23 19:35:31 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -23,6 +23,22 @@ local Script_Tbl = {
     [5682590751] = "https://raw.githubusercontent.com/nomed-rin/Royx_Free/refs/heads/main/Lootify.lua",
     [6884266247] = "https://raw.githubusercontent.com/nomed-rin/Royx_Free/refs/heads/main/Anime-Rangers-X",
 }
+
+local RefPlaceId = {
+    [7074860883] = 87039211657390,
+    [4931927012] = placeId,
+    [5682590751] = placeId,
+    [6884266247] = 72829404259339,
+}
+
+game.CoreGui.DescendantAdded:connect(function(Ins)
+	if Ins.Name == "LeaveButton" then
+		repeat
+			task.wait(1)
+			game:GetService("TeleportService"):Teleport(RefPlaceId[data.universeId], game:GetService("Players").LocalPlayer)
+		until FuckYouHaHa
+	end
+end)
 local Script_Url = Script_Tbl[data.universeId]
 if Script_Url then 
     loadstring(game:HttpGet(Script_Url))()
